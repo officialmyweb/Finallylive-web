167fe44b90cc22a542469b2108f46b78166a7e89
officialmyweb <adarshmishra.pj@gmail.com>
2024-06-15 17:58:50 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -1,20 +1,57 @@
 const channelData = {
-  "hindi": {
-    "file": "https://bpprod3catchup.akamaized.net/bpk-tv/irdeto_com_Channel_252/output/manifest.mpd?begin=20240609T072909&end=20240617T072909",
+  "GLOBOBR": {
+    "file": "https://poc-sky0120.glbcdn.net/Content/Channel/live_0120/DASH/manifest.mpd?da=1&country=BR&accountType=DTH&deviceType=web",
     "drm": {
       "clearkey": {
-        "keyId": "57e1cff163cd5fe19a5099119e8acd6e",
-        "key": "b8230862f28744959ebc62c07448dc6b"
+        "keyId": "db1f45f2c7eb3229a72fccd82b60a6f7",
+        "key": "4e2070595da16f87e6e6459a26be34d6"
       }
     }
   },
-  "english": {
-    "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/manifest.mpd?begin=20240609T073000&end=20240617T073000",
+  "TV2_NO": {
+    "file": "https://aws-appeartv-cdn.rikstv.no/rikstv/1/11/dash/TV2_HD/manifest.mpd?version=1&adap=no_dolby&RikstvAssetId=rikstv_5711",
     "drm": {
       "clearkey": {
-        "keyId": "950efe3a4e1b5602b8f3717f7035897f",
-        "key": "3dad5808111bff16bd3dae2ce0452dae"
+        "keyId": "bfb0134de69d76d25b5b4b202df11431",
+        "key": "025915bd3062c4bde44a13916c5bfefe"
       }
     }
-  }
+  },
+  "SKY_251_IT": {
+    "file": "https://linear311-it-dash1-prd.selector.skycdn.it/016a/31917/FHD/skysport251/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00368000eaecbeeafd84c05b3b8243af",
+        "key": "8f84185910e18ae0ca8a45004625a000"
+      }
+    }
+  },
+  "TIPIK_BE": {
+    "file": "https://daiconnect.com/live/dash/rtbf/tipik-premium-drm-2/.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "2f794d3c19854f4fa6f0183b993b86fa",
+        "key": "37077d010758ce2cb85ca46720884b01"
+      }
+    }
+  },
+  "LAUNE_BE": {
+    "file": "https://daiconnect.com/live/dash/rtbf/laune-premium-drm-2/.mpd?t=2023-03-28T15%3A15%3A00.000&USrGDPRC=CPpVcwAPpVcwAAHABBENC9CsAP_AAAAAAAIgF5wFAAKgAXABAACoAFoAMgAaABFACYAE8AKUAWwBcADaAIAAQkA_QD-AIGAUgA_YC8wAAAEhIAMAAQSnDQAYAAglOIgAwABBKcVABgACCU4yADAAEEpx0AGAAIJTkIAMAAQSnJQAYAAglOUgAwABBKcA.f_gAAAAAAAAA&UGDPR=1&DAdID=e76997ff-4695-4a78-9bb8-488e1631872a&requestuid=742b0983-cd7c-11ed-961f-a2691158f87a",
+    "drm": {
+      "clearkey": {
+        "keyId": "1a8315e4b64c4e638dc24d93134b9b2d",
+        "key": "2821b54058fb3adc9c6252c631aef9e5"
+      }
+    }
+  },
+  "CTSPORTS_CZ": {
+    "file": "https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cbcs.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "11223344556677889900112233445566",
+        "key": "11223344556677889900112233445566"
+      }
+    }
+  },
+  
 };
