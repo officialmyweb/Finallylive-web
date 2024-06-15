7a9b410470a9c75626654c6298b27f9c96d6f260
officialmyweb <adarshmishra.pj@gmail.com>
2024-06-15 17:33:21 +0530

    Create channel.js

---
+++ channel.js
@@ -0,0 +1,20 @@
+const channelData = {
+  "Channel Name - Hindi": {
+    "file": "https://bpprod3catchup.akamaized.net/bpk-tv/irdeto_com_Channel_252/output/manifest.mpd?begin=20240609T072909&end=20240617T072909",
+    "drm": {
+      "clearkey": {
+        "keyId": "57e1cff163cd5fe19a5099119e8acd6e",
+        "key": "b8230862f28744959ebc62c07448dc6b"
+      }
+    }
+  },
+  "Channel Name - English": {
+    "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/manifest.mpd?begin=20240609T073000&end=20240617T073000",
+    "drm": {
+      "clearkey": {
+        "keyId": "950efe3a4e1b5602b8f3717f7035897f",
+        "key": "3dad5808111bff16bd3dae2ce0452dae"
+      }
+    }
+  }
+};
